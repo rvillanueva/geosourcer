7019d135220feb4c806f55601bbd1193b088fef4
Ryan Villanueva <ryanvill@gmail.com>
2016-11-13 10:27:58 -0500

    UI updates

--- client/app/browse/browse.component.ts
+++ client/app/browse/browse.component.ts
@@ -9,14 +9,19 @@ export class BrowseComponent {
   $http;
   challenges;
   /*@ngInject*/
-  constructor($http) {
+  constructor($http, appConfig) {
     this.$http = $http;
     this.challenges;
     this.getChallenges();
+    this.mapboxKey = appConfig.mapboxKey;
   }
   getChallenges(){
     this.$http.get('/api/challenges').success(challenges => {
       this.challenges = challenges;
+      this.challenges.forEach((challenge, c) => {
+        challenge.backgroundImageSrc = 'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/' + challenge.search.targets[0].geo.lat+ ',' + challenge.search.targets[0].geo.lng + ',14.25,0,0/400x400?access_token=' + this.mapboxKey;
+      })
+      console.log(this.challenges)
     })
     .error(err => {
       console.log(err)
